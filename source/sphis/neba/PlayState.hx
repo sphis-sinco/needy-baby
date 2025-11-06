e153201c209231f1bcf3d27e0d4a97403bc52fb0
Viwu <sinconsistencia@gmail.com>
2025-11-05 19:48:50 -0600

    debug info

--- source/sphis/neba/PlayState.hx
+++ source/sphis/neba/PlayState.hx
@@ -1,12 +1,16 @@
 package sphis.neba;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.text.FlxText;
 import flixel.util.FlxColor;
 
 class PlayState extends FlxState
 {
 	public var worldTicks:Int = 0;
+
+	public var debugText:FlxText;
 
 	var baby:FlxSprite;
 
@@ -19,6 +23,14 @@ class PlayState extends FlxState
 		baby.screenCenter();
 
 		add(baby);
+
+		debugText = new FlxText();
+
+		debugText.x = 2;
+		debugText.y = 2;
+		debugText.fieldWidth = FlxG.width - 2;
+
+		add(debugText);
 	}
 
 	override function update(elapsed:Float)
@@ -26,5 +38,18 @@ class PlayState extends FlxState
 		super.update(elapsed);
 
 		worldTicks += 1;
+
+		debugText.visible = FlxG.keys.pressed.F3;
+		if (FlxG.keys.pressed.F3)
+		{
+			renderDebugInfo();
+		}
+	}
+
+	public function renderDebugInfo()
+	{
+		debugText.text = "Neba v" + Constants.APP_VERSION;
+
+		debugText.text += "\n\nWorld Ticks: " + worldTicks;
 	}
 }
