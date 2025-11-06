3ce4f177dca35023a6dea83231ae97c433daf69d
Viwu <sinconsistencia@gmail.com>
2025-11-05 19:42:39 -0600

    the baby

--- source/sphis/neba/PlayState.hx
+++ source/sphis/neba/PlayState.hx
@@ -1,12 +1,22 @@
 package sphis.neba;
 
+import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.util.FlxColor;
 
 class PlayState extends FlxState
 {
+	var baby:FlxSprite;
+
 	override function create()
 	{
 		super.create();
+
+		baby = new FlxSprite();
+		baby.makeGraphic(128, 128, FlxColor.RED);
+		baby.screenCenter();
+
+		add(baby);
 	}
 
 	override function update(elapsed:Float)
